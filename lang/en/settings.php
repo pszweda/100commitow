ac84bac49d613b7593348fec8dddb75ee5afca79
pszweda <piotr@szweda.com.pl>
2024-03-18 23:37:45 +0100

    translations

---
+++ lang/en/settings.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'heading' => 'Settings',
+    'menu' => [
+        'general' => 'General',
+        'services' => 'Services',
+        'goals' => 'Goals',
+        'pricing' => 'Pricing',
+    ],
+    'pages' => [
+        'general' => 'General',
+        'services' => 'Services',
+        'goals' => 'Goals',
+        'pricing' => 'Pricing',
+    ]
+];
